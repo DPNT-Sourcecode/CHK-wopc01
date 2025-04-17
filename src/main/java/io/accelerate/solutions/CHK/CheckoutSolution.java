bdba63dfa2fc8a45db6a4629942d89b9762dd815
sbx_user1051 <sbx_user1051@169.254.65.147>
2025-04-17 15:50:49 +0000

    Thu Apr 17 15:50:49 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -1,11 +1,21 @@
 package io.accelerate.solutions.CHK;
 
+import java.util.HashMap;
+
 import io.accelerate.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         //Check format of string to check valid
         //Check correct value
+        HashMap<Character,Integer> catalogueValue = new HashMap<>();
+        int current
+        for (char item : skus.toCharArray()) {
+            
+        }
+
+        return -1;
     }
 }
+
 
