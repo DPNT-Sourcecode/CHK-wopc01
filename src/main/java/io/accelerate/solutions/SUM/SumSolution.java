8fdd892e91372521470c38c50b9df8e98d4688b4
sbx_user1051 <sbx_user1051@169.254.54.81>
2025-04-17 12:14:15 +0000

    Thu Apr 17 12:14:15 UTC 2025

--- src/main/java/io/accelerate/solutions/SUM/SumSolution.java
+++ src/main/java/io/accelerate/solutions/SUM/SumSolution.java
@@ -5,10 +5,15 @@ import io.accelerate.runner.SolutionNotImplementedException;
 @SuppressWarnings("unused")
 public class SumSolution {
 
+    // sum(integer, integer) -> integer
+    // - param[0] = a positive integer between 0-100
+    // - param[1] = a positive integer between 0-100
+    // - @return = an integer representing the sum of the two numbers
 
-    public int sum(int x, int y) {
-        throw new SolutionNotImplementedException();
+    public int compute(int x, int y) {
+        return x+y;
     }
 
 }
+
 
