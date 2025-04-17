7c88913a0f5dd50912062953e6f97a8639683cbf
sbx_user1051 <sbx_user1051@169.254.65.147>
2025-04-17 17:51:06 +0000

    Thu Apr 17 17:51:06 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -17,36 +17,33 @@ public class CheckoutSolution {
         int currentBasketValue = 0;
 
         for (char item : skus.toCharArray()) {
-            if (item == 'A')
-                aCounter++;
-            if (item == 'B')
-                bCounter++;
-            if (item == 'E')
-                eCounter++;
-            if (item == 'F')
-                fCounter++;
-
-            currentBasketValue += catalogueValues.get(item);
+            productCatalog.getProduct(item).amount++;
         }
 
-        // E discount
-        currentBasketValue -= Math.min((eCounter / 2), bCounter) * 30;
-        bCounter -= eCounter / 2;
+        // // E discount
+        // currentBasketValue -= Math.min((eCounter / 2), bCounter) * 30;
+        // bCounter -= eCounter / 2;
 
-        // A discount
-        currentBasketValue -= (aCounter / 5) * 50;
-        currentBasketValue -= ((aCounter % 5) / 3) * 20;
+        // // A discount
+        // currentBasketValue -= (aCounter / 5) * 50;
+        // currentBasketValue -= ((aCounter % 5) / 3) * 20;
 
-        // B discount
-        if (bCounter > 0) {
-            currentBasketValue -= bCounter / 2 * 15;
-        }
+        // // B discount
+        // if (bCounter > 0) {
+        // currentBasketValue -= bCounter / 2 * 15;
+        // }
 
-        // F discount
-        currentBasketValue -= (fCounter / 3) * 10;
+        // // F discount
+        // currentBasketValue -= (fCounter / 3) * 10;
 
         return currentBasketValue;
     }
 
+    public void applyFreeDeal(Product mainProduct, Product dealProduct, int dealProductAmount) {
+        (mainProduct.getAmount()/dealProductAmount 1) * free
+
+    }
+
 }
+
 
