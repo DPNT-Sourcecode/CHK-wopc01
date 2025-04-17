9c0c25f26769bdec46b3853b8173c965487bd343
sbx_user1051 <sbx_user1051@169.254.56.183>
2025-04-17 12:52:12 +0000

    Thu Apr 17 12:52:12 UTC 2025

--- src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
+++ src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
@@ -3,15 +3,12 @@ package io.accelerate.solutions.HLO;
 import io.accelerate.runner.SolutionNotImplementedException;
 
 public class HelloSolution {
-    
+
     public String hello(String friendName) {
         return String.format("Hello, %s!", friendName);
     }
 
-    public static void main(String[] args) {
-        HelloSolution helloSolution = new HelloSolution();
-        System.out.println(helloSolution.hello("jim"));
-    }
 }
+
 
 
