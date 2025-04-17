d9c2a6af4e7c4ca933bdea38302bf466c2292246
sbx_user1051 <sbx_user1051@169.254.54.81>
2025-04-17 12:27:32 +0000

    Thu Apr 17 12:27:32 UTC 2025

--- src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
+++ src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
@@ -1,7 +1,10 @@
 package io.accelerate.solutions.HLO;
 
+import io.accelerate.runner.SolutionNotImplementedException;
+
 public class HelloSolution {
     public String hello(String friendName) {
-        return "Hello world";
+        return friendName;
     }
 }
+
--- src/main/java/io/accelerate/solutions/SUM/SumSolution.java
+++ src/main/java/io/accelerate/solutions/SUM/SumSolution.java
@@ -5,13 +5,9 @@ import io.accelerate.runner.SolutionNotImplementedException;
 @SuppressWarnings("unused")
 public class SumSolution {
 
-    // sum(integer, integer) -> integer
-    // - param[0] = a positive integer between 0-100
-    // - param[1] = a positive integer between 0-100
-    // - @return = an integer representing the sum of the two numbers
-
     public int compute(int x, int y) {
         return x+y;
     }
 
 }
+
